b0912e3c72eacd88097c2f938e54c56651d42fd7
Abhii Naithani <abhinaithani2@gmail.com>
2022-12-09 15:01:41 +0530

    Bottom navbar and tabbar added

--- App.js
+++ App.js
@@ -1,40 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import React from 'react';
-import AddNewProduct from './components/Catgor';
-import AddNewProduct2 from './components/AddProduct2';
-import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-//import BottomNav from './src/BottomNavbar' 
-import Home from './components/Home'
-import OrdersCard from './components/OrderCard';
-import OrdersList from './components/OrdersList';
-import NewProductscreen from './components/NewProduct_screen1';
-import ProductListScreen from './components/productList_screen';
-import CategoryScreen from './components/CategoryScreen';
-const Stack = createNativeStackNavigator();
-function App (){     
-  return (    
-  //    <NavigationContainer>
-  //    <Stack.Navigator>
-  //      <Stack.Screen name="Home" component={Home} />
-  //    </Stack.Navigator>
-  //  </NavigationContainer>
- //<BottomNav/>
- //<Home/>
- //<OrdersList/>
- //<AddNewProduct/>
- //<ProductListScreen/>
-<NewProductscreen/>
- //<AddNewProduct2/>
- //<CategoryScreen/>
- );
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {useWindowDimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import Home from './src/Screens/Home';
+import OrderScreen from './src/Screens/OrderScreen';
+const Tab = createMaterialBottomTabNavigator();
+function App() {
+  const layout = useWindowDimensions();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        activeColor="#7465B6"
+        barStyle={{backgroundColor: 'white'}}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <Icon name="home" color={color} size={layout.width * 0.05} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Orders"
+          component={OrderScreen}
+          options={{
+            tabBarLabel: 'Orders',
+            tabBarIcon: ({color}) => (
+              <Icon name="box-open" color={color} size={layout.width * 0.05} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Listings"
+          component={OrderScreen}
+          options={{
+            tabBarLabel: 'Listings',
+            tabBarIcon: ({color}) => (
+              <Icon name="list-alt" color={color} size={layout.width * 0.05} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Returns"
+          component={OrderScreen}
+          options={{
+            tabBarLabel: 'Returns',
+            tabBarIcon: ({color}) => (
+              <Icon
+                name="truck-loading"
+                color={color}
+                size={layout.width * 0.05}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={OrderScreen}
+          options={{
+            tabBarLabel: 'Notifications',
+            tabBarIcon: ({color}) => (
+              <Icon name="bell" color={color} size={layout.width * 0.05} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
-
-
 export default App;
---
+++ src/Screens/OrderScreen.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import {View, useWindowDimensions, SafeAreaView} from 'react-native';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+import OrdersList from '../components/OrdersList';
+
+const PendingRoute = () => (
+  <SafeAreaView>
+    <OrdersList />
+  </SafeAreaView>
+);
+
+const ReadyToShipRoute = () => (
+  <View style={{flex: 1, backgroundColor: '#673ab7'}} />
+);
+
+const ShippedRoute = () => (
+  <View style={{flex: 1, backgroundColor: '#673ab7'}} />
+);
+
+const CancelledRoute = () => (
+  <View style={{flex: 1, backgroundColor: '#673ab7'}} />
+);
+
+export default function OrderScreen() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {key: 'first', title: 'Pending'},
+    {key: 'second', title: 'Ready To Ship'},
+    {key: 'third', title: 'Shipped'},
+    {key: 'fourth', title: 'Cancelled'},
+  ]);
+
+  const renderScene = SceneMap({
+    first: PendingRoute,
+    second: ReadyToShipRoute,
+    third: ShippedRoute,
+    fourth: CancelledRoute,
+  });
+
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={{backgroundColor: 'white'}}
+      style={{backgroundColor: '#3F2B96'}}
+      labelStyle={{
+        fontFamily: 'Montserrat',
+        fontWeight: '600',
+        fontSize: layout.width * 0.03055,
+        textAlign: 'center',
+      }}
+    />
+  );
+
+  return (
+    <TabView
+      renderTabBar={renderTabBar}
+      navigationState={{index, routes}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{width: layout.width}}
+      style={{backgroundColor: 'black'}}
+    />
+  );
+}
